e4ff0447f3ae573e9767bdc018e53534c6ae3b3e
eduar <eduardvitko>
2022-10-15 22:59:50 +0300

    setting Authenticated and create User class and adding import

---
+++ src/app/model/user.ts
@@ -0,0 +1,13 @@
+export class User{
+  public name: string ;
+  public username: string;
+  public password: string;
+  public role: [] ;
+
+  constructor() {
+    this.name = '';
+    this.username = '';
+    this.password = '';
+    this.role = [];
+  }
+}
--- src/app/service/authenticated.service.ts
+++ src/app/service/authenticated.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import {Observable} from "rxjs";
+import {User} from "../model/user";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticatedService {
+  public host = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http : HttpClient) {}
+  public login(user: User): Observable<HttpResponse<User>> {
+    return this.http.post<User>(`${this.host}/api/login`, user, { observe: 'response' });
+  }
 }
