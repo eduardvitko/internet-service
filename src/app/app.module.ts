10700b916f91c66881267621ca1c09695f3172c9
eduar <eduardvitko>
2022-10-19 17:34:02 +0300

    create app-routing.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AuthenticatedService} from "./service/authenticated.service";
 
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import {AuthenticationGuard} from "./guard/authentication.guard";
+
+import {AuthInterceptor} from "./interceptor/auth.interceptor";
+import {NotificationService} from "./service/notification.service";
+import {UserService} from "./service/user.service";
+import {AppRoutingModule} from "./app-routing.module";
+import {NotificationModule} from "./notification.module";
+import {FormsModule} from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
@@ -13,9 +23,15 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    NotificationModule
+
+
   ],
-  providers: [AuthenticatedService],
+    providers: [NotificationService, AuthenticationGuard, AuthenticatedService, UserService,
+      { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
